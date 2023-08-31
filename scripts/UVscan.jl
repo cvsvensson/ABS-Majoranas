fc09c7b15bc61dedc9671a42395b29e627b103f3
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-08-31 16:15:30 +0200

    rename target to cost

--- scripts/UVscan.jl
+++ scripts/UVscan.jl
@@ -4,13 +4,13 @@ include(srcdir("abs_chain_misc.jl"))
 
 fixedparams = (; Δ=1.0, t=0.5, tratio=0.2, h=1.5) 
 MaxTime = 10 # Maximum time in seconds for each sweet spot optimization 
-target = MPU # Choose LD, MP or MPU
+cost = MPU # Choose LD, MP or MPU
 res = 20 #Resolution 
 us = range(0, 5, length=res)
 vs = range(0, 1, length=res)
 
-results = sweet_spot_scan((us, :U), (vs, :V), anti_parallel_sweet_spot; fixedparams, MaxTime, target);
+results = sweet_spot_scan((us, :U), (vs, :V), anti_parallel_sweet_spot; fixedparams, MaxTime, cost);
 tagsave(datadir("UV-scan","anti_parallel", savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
 
-results = sweet_spot_scan((us, :U), (vs, :V), parallel_sweet_spot; fixedparams, MaxTime, target);
+results = sweet_spot_scan((us, :U), (vs, :V), parallel_sweet_spot; fixedparams, MaxTime, cost);
 tagsave(datadir("UV-scan","parallel", savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
--- scripts/Uhscan.jl
+++ scripts/Uhscan.jl
@@ -4,12 +4,12 @@ include(srcdir("abs_chain_misc.jl"))
 
 fixedparams = (; Δ=1.0, t=0.5, tratio=0.2, V=0) 
 MaxTime = 10 # Maximum time in seconds for each sweet spot optimization 
-target = MPU # Choose LD, MP or MPU
+cost = MPU # Choose LD, MP or MPU
 res = 20 #Resolution 
 us = range(0, 5, length=res)
 hs = range(0, 6, length=res)
-results = sweet_spot_scan((us, :U), (hs, :h), anti_parallel_sweet_spot; fixedparams, MaxTime, target);
+results = sweet_spot_scan((us, :U), (hs, :h), anti_parallel_sweet_spot; fixedparams, MaxTime, cost);
 tagsave(datadir("Uh-scan","anti_parallel", savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
 
-results = sweet_spot_scan((us, :U), (hs, :h), parallel_sweet_spot; fixedparams, MaxTime, target);
+results = sweet_spot_scan((us, :U), (hs, :h), parallel_sweet_spot; fixedparams, MaxTime, cost);
 tagsave(datadir("Uh-scan","parallel", savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
