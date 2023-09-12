7dd0e534562444beaf072e1fb0ac194df47e77e2
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-09-12 11:46:03 +0200

    exps kwarg

--- scripts/Uhscan.jl
+++ scripts/Uhscan.jl
@@ -8,7 +8,8 @@ target = MPU # Choose LD, MP or MPU
 res = 20 #Resolution 
 us = range(0, 5, length=res)
 hs = range(0, 6, length=res)
-results = sweet_spot_scan((us, :U), (hs, :h), anti_parallel_sweet_spot; fixedparams, MaxTime, target);
+exps = collect(range(.5,3,3))
+results = sweet_spot_scan((us, :U), (hs, :h), anti_parallel_sweet_spot; fixedparams, MaxTime, target, exps);
 tagsave(datadir("Uh-scan","anti_parallel", savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
 
 results = sweet_spot_scan((us, :U), (hs, :h), parallel_sweet_spot; fixedparams, MaxTime, target);
