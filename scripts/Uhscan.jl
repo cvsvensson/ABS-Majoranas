de8d17b2dc4895bf7c8d61321b7f5a199615ff34
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-09-13 15:41:15 +0200

    include method in savename

--- scripts/Uhscan.jl
+++ scripts/Uhscan.jl
@@ -4,17 +4,18 @@ include(srcdir("abs_chain_misc.jl"))
 
 V = parse(Float64, ARGS[1])
 fixedparams = (; Δ=1.0, t=0.5, tratio=0.2, V) 
-MaxTime = 30 # Maximum time in seconds for each sweet spot optimization 
+MaxTime = 20 # Maximum time in seconds for each sweet spot optimization 
 target = MPU # Choose LD, MP or MPU
-res = 40 #Resolution 
+res = 20 #Resolution 
 us = range(0, 5, length=res)
 hs = range(0, 6, length=res)
-exps = collect(range(0.5, 4, length=4))
-Methods = [:generating_set_search, :probabilistic_descent, :adaptive_de_rand_1_bin_radiuslimited]
+exps = collect(range(0.5, 3, length=4))
+Methods = [:probabilistic_descent, :probabilistic_descent, :generating_set_search]
+#Methods = [:dxnes, :xnes, :de_rand_1_bin_radiuslimited, :adaptive_de_rand_1_bin_radiuslimited, :generating_set_search, :adaptive_de_rand_1_bin, :separable_nes, :probabilistic_descent, :resampling_memetic_search, :resampling_inheritance_memetic_search]
 
-for Method in Methods
+for (n,Method) in enumerate(Methods)
     results = sweet_spot_scan((us, :U), (hs, :h), anti_parallel_sweet_spot; fixedparams, MaxTime, target, exps, Method)
-    tagsave(datadir("Uh-scan", "anti_parallel", string(Method), savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
+    tagsave(datadir("Uh-scan", "anti_parallel", "methods", savename("$n",results, "jld2"; allowedtypes=(Number, String, NamedTuple, Symbol))), results)
 end
 #results = sweet_spot_scan((us, :U), (hs, :h), parallel_sweet_spot; fixedparams, MaxTime, target);
 #tagsave(datadir("Uh-scan","parallel", savename(results, "jld2"; allowedtypes=(Number, String, NamedTuple))), results)
