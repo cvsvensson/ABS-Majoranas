b32ceb1356abc2f10f7e200d6c48d8bbdd573929
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-09-21 14:33:01 +0200

    remove stuff, improve tsl

--- scripts/TSL.jl
+++ scripts/TSL.jl
@@ -1,45 +1,68 @@
-using QuantumDots
+
+using DrWatson
+@quickactivate "Majorana sweet spot"
+includet(srcdir("abs_chain_misc.jl"))
+includet(srcdir("plotting.jl"))
 
 d = FermionBasis((:L, :C, :R), (:↑, :↓); qn=QuantumDots.parity)
 ##
 Δ = 1.0
 t = 0.5Δ
+tsoc = 0.2t
+U = 5Δ
+h = 1.5Δ / 2
 ##
-transport = Transport(QuantumDots.Pauli(), (; T=1 / 40, μ=(0.0, 0.0), Γ=1 / 1))
-params = (; Δ, U=5Δ, t, tsoc=0.2t, h=1.5Δ / 2)
-ham(μL, μC, μR) = blockdiagonal(QuantumDots.TSL_hamiltonian(d; params..., μC, μL, μR), d)
+transport = Transport(QuantumDots.Pauli(), (; T=1 / 40, μ=(0.0, 0.0), Γ=1))
+params = (; Δ, U, t, tsoc, h)
+hamLCR(μL, μC, μR) = blockdiagonal(QuantumDots.TSL_hamiltonian(d; params..., μC, μL, μR), d)
 
 ## Fig3
 res = 50
 μ1s = range(-1, 1, res)
 μ2s = range(-1, 1, res)
-sols3 = [[solve(ham(μ1, μC, μ2); basis=d) for μ1 in μ1s, μ2 in μ2s] for μC in (-.313, -.7, 0.0 ) .* params.Δ]
+sols3 = [[solve(hamLCR(μ1, μC, μ2); basis=d) for μ1 in μ1s, μ2 in μ2s] for μC in (-0.313, -0.7, 0.0) .* params.Δ]
 ##
-f = Figure(; resolution = 400 .* (3,1), fontsize=25);
-g = f[1,1] = GridLayout();
-hms = [heatmap(g[1,n], μ1s,μ2s, map(ss->ss.gap, data), colormap=:redsblues, colorrange=0.8 .* (-1, 1)) for (n, data) in enumerate(sols3)]
-Colorbar(g[1,end+1], hms[1].plot)
+f = Figure(; resolution=400 .* (3, 1), fontsize=25);
+g = f[1, 1] = GridLayout();
+hms = [heatmap(g[1, n], μ1s, μ2s, map(ss -> ss.gap, data), colormap=:redsblues, colorrange=0.8 .* (-1, 1)) for (n, data) in enumerate(sols3)]
+Colorbar(g[1, end+1], hms[1].plot)
 f
 ##
 res = 50
 μ1s = range(-1, 1, res)
 μ2s = range(-1, 1, res)
-ham(μL, μR) = blockdiagonal(QuantumDots.TSL_hamiltonian(d; params..., μC=-0.558 * params.Δ, μL, μR), d)
-sols = [solve(ham(μ1, μ2); basis=d, transport) for μ1 in μ1s, μ2 in μ2s]
+hamLR(μL, μR) = blockdiagonal(QuantumDots.TSL_hamiltonian(d; params..., μC=-0.558 * params.Δ, μL, μR), d)
+sols = [solve(hamLR(μ1, μ2); basis=d, transport) for μ1 in μ1s, μ2 in μ2s]
 
 ##
-heatmap(μ1s, μ2s, map(ss -> ss.conductance[1, 2], sols); colormap=:vik, colorrange=pi * transport.parameters.Γ^2 .* (-1, 1))
+heatmap(μ1s, μ2s, map(ss -> ss.conductance[1, 2], sols); colormap=:redsblues, colorrange=pi * transport.parameters.Γ^2 .* (-1, 1))
 heatmap(μ1s, μ2s, map(ss -> ss.mps.left.mpu, sols); colormap=:viridis, colorrange=(0, 1))
-heatmap(μ1s, μ2s, map(ss -> ss.gap, sols); colormap=:vik, colorrange= 0.8 .* (-1, 1))
+heatmap(μ1s, μ2s, map(ss -> ss.gap, sols); colormap=:redsblues, colorrange=0.8 .* (-1, 1))
 
-##
-res = 100
-μs = range(-.4, .4, res)
+
+## Let's make Figure 2
+res = 50
+μs = range(-0.4, 0.4, res)
 μCs = range(-1, 1, res)
-ham(μ, μC) = blockdiagonal(QuantumDots.TSL_hamiltonian(d; params..., μC, μL=μ, μR=μ), d)
-sols = [solve(ham(μ, μC); basis=d) for μC in μCs, μ in μs]
-heatmap(μCs, μs, map(ss -> ss.mps.left.mpu, sols); colormap=:viridis, colorrange=(0, 1))
-heatmap(μCs, μs, map(ss -> (ss.gap), sols); colormap=:vik, colorrange= 0.1 .* (-1, 1))
-
-
+hamGC(μG, μC) = blockdiagonal(QuantumDots.TSL_hamiltonian(d; params..., μC, μL=μG, μR=μG), d)
+fig2data = [solve(hamGC(μ, μC); basis=d) for μC in μCs, μ in μs]
 ##
+fig2 = Figure();
+fig2g = fig2[1, 1] = GridLayout()
+fig2ax1 = Axis(fig2g[1, 1]; xlabel=L"\mu_C", ylabel=L"\mu")
+fig2ax2 = Axis(fig2g[2, 1]; xlabel=L"\mu_C", ylabel=L"\mu")
+heatmap!(fig2ax1, μCs, μs, map(ss -> (ss.gap), fig2data); colormap=:redsblues, colorrange=0.1 .* (-1, 1))
+heatmap!(fig2ax2, μCs, μs, map(ss -> ss.mps.left.mpu, fig2data); colormap=:viridis, colorrange=(0, 1))
+fig2
+## Add in optimized sweet spot
+function tsl_cost(x; P=10^3)
+    μ, μC = x
+    sol = solve(hamGC(μ, μC); basis=d)
+    P * abs2(sol.gap) + MPU(sol)
+end
+opt = bboptimize(tsl_cost; NumDimensions=2, Method=:probabilistic_descent, MaxTime=5)
+ss = best_candidate(opt)
+##
+scatter!(fig2ax1, [ss[2]], [ss[1]])
+scatter!(fig2ax2, [ss[2]], [ss[1]])
+fig2
--- scripts/plotting.jl
+++ scripts/plotting.jl
@@ -3,72 +3,55 @@ using DrWatson
 includet(srcdir("abs_chain_misc.jl"))
 includet(srcdir("plotting.jl"))
 using DataFrames
-resultsUVp = collect_results(datadir("UV-scan", "parallel"))
-resultsUhp = collect_results(datadir("Uh-scan", "parallel"))
+# resultsUVp = collect_results(datadir("UV-scan", "parallel"))
+# resultsUhp = collect_results(datadir("Uh-scan", "parallel"))
 # Methods = [:generating_set_search, :probabilistic_descent, :adaptive_de_rand_1_bin_radiuslimited]
-resultsUVap = collect_results(datadir("UV-scan", "anti_parallel","inhomo")) 
-resultsUhap = collect_results(datadir("Uh-scan", "anti_parallel","inhomo"))
+resultsUVap = collect_results(datadir("UV-scan", "anti_parallel", "final"))
+resultsUhap = collect_results(datadir("Uh-scan", "anti_parallel", "final"))
 
-resultsUVapborg = collect_results(datadir("UV-scan", "anti_parallel","borg"))
-resultsUhapborg = collect_results(datadir("Uh-scan", "anti_parallel","borg"))
-resultsUVapborg2 = collect_results(datadir("UV-scan", "anti_parallel","borg2"))
-resultsUhapborg2 = collect_results(datadir("Uh-scan", "anti_parallel","borg2"))
-
-
-## UV
-UVpfig, _, _ = plot_sweet_scan(resultsUVp[1, :])
-display(UVpfig)
-UVapfig, _, _ = plot_sweet_scan(resultsUVap[1, :])
-display(UVapfig)
-## Uh
-Uhpfig, _, _ = plot_sweet_scan(resultsUhp[1, :])
-display(Uhpfig)
-Uhapfig, _, _ = plot_sweet_scan(resultsUhap[1, :])
-display(Uhapfig)
-##
-
-fig, _, _ = plot_sweet_scan(resultsUhap[1, :]; datamap=x -> x.parameters.μ1 - x.parameters.U - x.parameters.h, colorscale=identity, colorrange=(-2, 2), colorbar=false)
-display(fig)
-fig, _, _ = plot_sweet_scan(resultsUhap[1, :]; datamap=x -> x.parameters.μ2 + x.parameters.h, colorscale=identity, colorrange=(-2, 2), colorbar=false)
-display(fig)
-fig, _, _ = plot_sweet_scan(resultsUhap[1, :]; datamap=x -> x.parameters.ϕ, colorscale=identity, colorrange=(0, pi), colorbar=false)
-display(fig)
-
-##
-    
-
-
-##
-# for n in 1:3
-scanfig = let data1 = resultsUhap[1, :], data2 = resultsUVap[1, :]
-    fig = Figure(; resolution=400 .* (2, 1), fontsize=20, backgroundcolor=:white)
-    xlabel1 = paramstyle[data1[:xlabel]]
-    xlabel2 = paramstyle[data2[:xlabel]]
-    ylabel1 = paramstyle[data1[:ylabel]]
-    ylabel2 = paramstyle[data2[:ylabel]]
-    ax1 = Axis(fig[1, 1]; xlabel=xlabel1, ylabel=ylabel1)
-    ax2 = Axis(fig[1, 2]; xlabel=xlabel2, ylabel=ylabel2)
-    g = fig[1, 1:3] = GridLayout()
-    hm1 = plot_sweet_scan!(ax1, data1)#; datamap = x->abs(x.gap), colorrange = (1e-5,1))
-    hm2 = plot_sweet_scan!(ax2, data2)#; datamap = x->abs(x.gap), colorrange = (1e-5,1))
-    label = L"1-\text{MP}"
-    add_exp_colorbar!(fig[1, 3], hm1;)
-    paramstring1 = map(x -> round.(x, digits=2), data1[:fixedparams])
-    paramstring2 = map(x -> round.(x, digits=2), data2[:fixedparams])
-
-    supertitle = Label(fig[1, 3, Top()], label, fontsize=25, padding=(0, 0, 8, 0))
-    supertitle2 = Label(fig[1, 1, Top()], string(data1[:sweet_spots][1].optimization.Method), fontsize=20, padding=(0, 0, 8, 0))
-    supertitle2 = Label(fig[1, 2, Top()], string(data2[:sweet_spots][1].optimization.Method), fontsize=20, padding=(0, 0, 8, 0))
-    colgap!(fig.layout, 1, 16)
-    colgap!(fig.layout, 2, 10)
-    #save(plotsdir(string("uhvplot_transparent", paramstring1, "_", paramstring2, ".png")), fig, px_per_unit=4)
-    fig|>display
+select_best_sweet_spot(spots...) = spots[findmin(MPU, spots)[2]]
+get_property(results, label) = map(x -> x[label], results)
+function combine_results(results)
+    testlabels = (:fixedparams, :xlabel, :ylabel, :target, :x, :y)
+    props = map(label -> unique(getindex.(eachrow(results), label)), testlabels)
+    @assert all(length.(props) .== 1)
+    uniqueprops = (; zip(testlabels, map(only, props))...)
+    sweet_spots = map(select_best_sweet_spot, results[:, :sweet_spots]...)
+    merge(uniqueprops, (; sweet_spots))
 end
-# end
+resultsUh = combine_results(resultsUhap)
+resultsUV = combine_results(resultsUVap)
+
+
+for n in 1:4
+    scanfig = let data1 = resultsUhap[n, :], data2 = resultsUVap[n, :]
+        fig = Figure(; resolution=400 .* (2, 1), fontsize=20, backgroundcolor=:white)
+        xlabel1 = paramstyle[data1[:xlabel]]
+        xlabel2 = paramstyle[data2[:xlabel]]
+        ylabel1 = paramstyle[data1[:ylabel]]
+        ylabel2 = paramstyle[data2[:ylabel]]
+        ax1 = Axis(fig[1, 1]; xlabel=xlabel1, ylabel=ylabel1)
+        ax2 = Axis(fig[1, 2]; xlabel=xlabel2, ylabel=ylabel2)
+        g = fig[1, 1:3] = GridLayout()
+        hm1 = plot_sweet_scan!(ax1, data1)#; datamap = x->abs(x.gap), colorrange = (1e-5,1))
+        hm2 = plot_sweet_scan!(ax2, data2)#; datamap = x->abs(x.gap), colorrange = (1e-5,1))
+        label = L"1-\text{MP}"
+        add_exp_colorbar!(fig[1, 3], hm1;)
+        paramstring1 = map(x -> round.(x, digits=2), data1[:fixedparams])
+        paramstring2 = map(x -> round.(x, digits=2), data2[:fixedparams])
+
+        supertitle = Label(fig[1, 3, Top()], label, fontsize=25, padding=(0, 0, 8, 0))
+        supertitle2 = Label(fig[1, 1, Top()], string(data1[:sweet_spots][1].optimization.Method), fontsize=20, padding=(0, 0, 8, 0))
+        supertitle2 = Label(fig[1, 2, Top()], string(data2[:sweet_spots][1].optimization.Method), fontsize=20, padding=(0, 0, 8, 0))
+        colgap!(fig.layout, 1, 16)
+        colgap!(fig.layout, 2, 10)
+        #save(plotsdir(string("uhvplot_transparent", paramstring1, "_", paramstring2, ".png")), fig, px_per_unit=4)
+        fig |> display
+    end
+end
 
 ##
-ssdata = resultsUhap[2, :]
-ssdata = resultsUhapborg[1, :]
+ssdata = resultsUh
 nx, ny = size(ssdata[:sweet_spots])
 smallres = 100
 positions = map(x -> Int.(floor.((nx, ny) .* x)), [(0.4, 0.7), (0.25, 0.5), (0.25, 0.38), (0.25, 0.33)])
@@ -77,6 +60,7 @@ sweet_spots = [ssdata[:sweet_spots][pos...] for pos in positions]
 paramstring = map(ss -> map(x -> round(x, digits=2), ss.parameters), sweet_spots)
 transport = Transport(QuantumDots.Pauli(), (; T=1 / 20, μ=(0.0, 0.0)))
 csdata_small = [charge_stability_scan((; ss.parameters...), 0.8, 0.8, smallres; transport) for ss in sweet_spots]
+
 
 ##
 colors = cgrad(:rainbow, categorical=true)[[1, 2, 5, 4][1:length(csdata_small)]]
@@ -98,24 +82,26 @@ scatter!(ax, map(ss -> ss.parameters[ssdata.xlabel], sweet_spots), map(ss -> ss.
     color=colors, markersize=20, marker=:xcross, strokewidth=2)
 
 datamap = x -> sign(x.gap)
-datamap2 = x -> real(x.conductance)[1, 2] * real(x.conductance)[1, 1]
-datamap2 = x -> real(x.conductance)[1, 2] - real(x.conductance)[2, 1]
-datamap2 = x -> real(x.conductance)[1, 2]
-datamap2 = MPU
+# datamap2 = x -> real(x.conductance)[1, 2] * real(x.conductance)[1, 1]
+# datamap2 = x -> real(x.conductance)[1, 2] - real(x.conductance)[2, 1]
+# datamap2 = MPU
+# datamap2 = x -> real(x.conductance[1, 2]) / real(x.conductance[1, 1])
 spinecolor = [NamedTuple(map(x -> x => colors[n], [:bottomspinecolor, :leftspinecolor, :topspinecolor, :rightspinecolor])) for n in eachindex(csdata_small)]
 small_axes = [Axis(gs[n, 1]; spinecolor[n]..., spinewidth=4, aspect=1) for (n, data) in enumerate(csdata_small)] #subtitle = L"ϕ=%$(round(ϕ,digits=2))" 
-small_axes2 = [Axis(gs[n, 2]; spinecolor[n]..., spinewidth=4, aspect=1) for (n, data) in enumerate(csdata_small)] #subtitle = L"ϕ=%$(round(ϕ,digits=2))" 
+# small_axes2 = [Axis(gs[n, 2]; spinecolor[n]..., spinewidth=4, aspect=1) for (n, data) in enumerate(csdata_small)] #subtitle = L"ϕ=%$(round(ϕ,digits=2))" 
 foreach((ax, data) -> plot_charge_stability!(ax, data; datamap), small_axes, csdata_small)
-foreach((ax, data) -> plot_charge_stability!(ax, data; datamap=datamap2, colormap=:vik), small_axes2, csdata_small)
-hidedecorations!.(small_axes)
-hidedecorations!.(small_axes2)
-labels = [Label(gs[n, 1:2, Bottom()], L"MP ≈ %$(round(1-MPU(ss),digits=2))", padding=(0, 0, -15, 4)) for (n, ss) in enumerate(sweet_spots)]
-Label(gs[1, 1, Top()], L"\text{Parity}", padding=(0, 0, 2, -10))
-Label(gs[1, 2, Top()], L"G_{\text{nl}}", padding=(0, 0, 2, -10))
+# foreach((ax, data) -> plot_charge_stability!(ax, data; datamap=datamap2, colormap=:vik), small_axes2, csdata_small)
+# hidedecorations!.(small_axes)
+# hidedecorations!.(small_axes2)
+labels = [Label(gs[n, 1, Bottom()], L"MP ≈ %$(round(1-MPU(ss),digits=2))", padding=(0, 0, -15, 4)) for (n, ss) in enumerate(sweet_spots)]
+# Label(gs[1, 1, Top()], L"\text{Parity}", padding=(0, 0, 2, -10))
+# Label(gs[1, 2, Top()], L"G_{\text{nl}}", padding=(0, 0, 2, -10))
+Label(gb[1, 1, TopLeft()], L"a)", padding=(0, 20, 0, -10), tellheight=false, tellwidth=false)
+Label(gs[1, 1, TopLeft()], L"b)", padding=(0, 20, 0, -10), tellheight=false, tellwidth=false)
 
 
 colsize!(g, 2, Auto(0.4))
-colsize!(g, 2, Auto(0.9))
+# colsize!(g, 2, Auto(0.9))
 cb.alignmode = Mixed(top=-15, bottom=0)
 f |> display
 
@@ -134,7 +120,7 @@ sweet_spots = [ssdata[:sweet_spots][pos...] for pos in positions]
 transport = Transport(QuantumDots.Pauli(), (; T=1 / 20, μ=(0.0, 0.0)))
 paramstring = map(ss -> map(x -> round(x, digits=2), ss.parameters), sweet_spots)
 
-csdata_small = [charge_stability_scan((; ss.parameters...,ϕ = ss.parameters.ϕ+0*1.5, μ1 = ss.parameters.μ1 + .2,μ2 = ss.parameters.μ2 + .2), 1.5, 1.5, smallres; transport) for ss in sweet_spots]
+csdata_small = [charge_stability_scan((; ss.parameters..., ϕ=ss.parameters.ϕ + 0 * 1.5, μ1=ss.parameters.μ1 + 0.2, μ2=ss.parameters.μ2 + 0.2), 1.5, 1.5, smallres; transport) for ss in sweet_spots]
 
 ##
 colors = cgrad(:rainbow, categorical=true)[[1, 2, 5, 4][1:length(csdata_small)]]
@@ -164,10 +150,10 @@ datamap2 = x -> real(x.conductance)[1, 2]
 spinecolor = [NamedTuple(map(x -> x => colors[n], [:bottomspinecolor, :leftspinecolor, :topspinecolor, :rightspinecolor])) for n in eachindex(csdata_small)]
 small_axes = [Axis(gs[n, 1]; spinecolor[n]..., spinewidth=4, aspect=1) for (n, data) in enumerate(csdata_small)] #subtitle = L"ϕ=%$(round(ϕ,digits=2))" 
 small_axes2 = [Axis(gs[n, 2]; spinecolor[n]..., spinewidth=4, aspect=1) for (n, data) in enumerate(csdata_small)] #subtitle = L"ϕ=%$(round(ϕ,digits=2))" 
-hms = map((ax, data) -> plot_charge_stability!(ax, data; datamap, colorrange = (1,1)), small_axes, csdata_small)
+hms = map((ax, data) -> plot_charge_stability!(ax, data; datamap, colorrange=(1, 1)), small_axes, csdata_small)
 # map((ax, data) -> contour!(ax, map(datamap,data[:data]); levels = [10.0^(-x) for x in 1:3 ], colorrange = (-1,1),colormap = :berlin), small_axes, csdata_small)
-foreach((n,hm)-> Colorbar(gs[n,3],hm), eachindex(hms),hms)
-map((ax, data) -> plot_charge_stability!(ax, data; datamap=datamap2, colormap=:vik, colorrange=.2), small_axes2, csdata_small)
+foreach((n, hm) -> Colorbar(gs[n, 3], hm), eachindex(hms), hms)
+map((ax, data) -> plot_charge_stability!(ax, data; datamap=datamap2, colormap=:vik, colorrange=0.2), small_axes2, csdata_small)
 
 hidedecorations!.(small_axes)
 hidedecorations!.(small_axes2)
